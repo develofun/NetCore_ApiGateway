8b8d23b4ced79db33a8a12902dfac634fcd630a6
daekyun <daekyunlee@haegin.kr>
2020-11-05 11:24:50 +0900

    인증이 필요한 api 요청 구분 및 처리

---
+++ Managers/AuthorizationManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace NetCore_Gateway.Managers
+{
+    public class AuthorizationManager
+    {
+        private static Dictionary<string, Dictionary<string, List<string>>> _dicAuth = new Dictionary<string, Dictionary<string, List<string>>>();
+
+        static AuthorizationManager()
+        {
+            _dicAuth = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, List<string>>>>(JsonAppConfig.Load<string>("authorization.json"));
+        }
+
+        public static bool CheckEndPointAuth(string endpoint, string method, string permission)
+        {
+            endpoint = endpoint.ToLower();
+            if (!_dicAuth.ContainsKey(endpoint)) return true;
+
+
+            var methodDict = _dicAuth[endpoint];
+            method = method.ToUpper();
+            if (!methodDict.ContainsKey(method)) return true;
+
+            return methodDict[method].Contains(permission);
+        }
+
+        public static bool CheckEndPointAuth(string endpoint, string method, string[] permissionsArray)
+        {
+            foreach (string permission in permissionsArray)
+            {
+                var check = CheckEndPointAuth(endpoint, method, permission);
+                if (check) return check;
+            }
+
+            return false;
+        }
+    }
+}
