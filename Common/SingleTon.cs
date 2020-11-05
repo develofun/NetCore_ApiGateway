6ae6399ef220bd8110a128d2a88b6f7f49a38d85
daekyun <daekyunlee@haegin.kr>
2020-11-05 11:23:36 +0900

    공용 코드 생성

---
+++ Common/SingleTon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace NetCore_Gateway
+{
+	public class Singleton<T> where T : class, new()
+	{
+		public static T Instance { get; protected set; }
+
+		static Singleton()
+		{
+			Instance = new T();
+		}
+	}
+}
